e5299817277c92810061566529c7aff5145bb7d3
easybird <easybird85@gmail.com>
2016-01-25 21:14:40 +0100

    add basic express app + routing + material design + jade

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Waterschildpadden' });
+});
+
+/* GET home page. */
+router.get('/*', function(req, res, next) {
+    res.render('mainPage', { title: 'Waterschildpadden' });
+});
+
+module.exports = router;
